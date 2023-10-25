cbce2975e6fe939109720ba4e9c3d1b8107db8b2
sachinp2908 <sachinpawar2542@gmail.com>
2023-10-25 13:01:14 +0530

    added one method

--- src/test/Login.java
+++ src/test/Login.java
@@ -4,4 +4,7 @@ public class Login {
 public void loginPageDetails() 
 {
 	System.out.println("Login page credentioals");}
+public void aPageDetails() 
+{
+	System.out.println("Login page credentioals");}
 }
